8cce10921a9cc73a86419b6563172fa949b49146
guavalines <konstrictor@gmail.com>
2022-09-05 23:38:06 -0700

    added noticed to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,13 @@ class Comment < ApplicationRecord
   before_destroy :cleanup_notifications
   has_noticed_notifications model_name: 'Notification'
 
+  private
+
+  def notify_recipient
+    CommentNotification.with(comment: self, post: post).deliver_later(post.user)
+  end
+
+  def cleanup_notifications
+    notifications_as_comment.destroy_all
+  end
 end

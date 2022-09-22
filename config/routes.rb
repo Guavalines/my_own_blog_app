ec84ec4916448f43de1d4a2bc7f2e88a08136f82
guavalines <konstrictor@gmail.com>
2022-09-21 22:17:02 -0700

    added comment counter to comment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
+  resources :projects
+  patch 'drag/project'
+  get 'members/dashboard'
+  resources :categories
   authenticated :user, ->(user) { user.admin? } do
     get 'admin', to: 'admin#index'
     get 'admin/posts'
     get 'admin/comments'
     get 'admin/users'
-    get 'admin/show_post/:id', to: "admin#show_post", as: 'admin_post'
+    get 'admin/post/:id', to: "admin#show_post", as: 'admin_post'
   end
 
   get 'search', to: 'search#index'
-  get 'users/profile'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   get '/u/:id', to: 'users#profile', as: 'user'
+  resources :after_signup
 
   # /posts/1/comments/4
   resources :posts do

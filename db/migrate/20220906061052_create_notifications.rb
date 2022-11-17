fd98b8510c5a6757ba6668a672b8bddf168cf987
guavalines <konstrictor@gmail.com>
2022-11-17 12:41:52 -0800

    fixed the issue with db postgresql switch, such as casting issue like string to text, and also brackets to parenthesis

--- db/migrate/20220906061052_create_notifications.rb
+++ db/migrate/20220906061052_create_notifications.rb
@@ -2,8 +2,8 @@ class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
     create_table :notifications do |t|
       t.references :recipient, polymorphic: true, null: false
-      t.string :type, null: false
-      t.json :params
+      t.text :type, null: false
+      t.jsonb :params
       t.datetime :read_at
 
       t.timestamps
